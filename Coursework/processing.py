d729658b0a452eca4e4082b7c536b5cc2a649dab
baranovva <144531586+baranovva@users.noreply.github.com>
2024-03-01 18:15:19 +0300

    Add files via upload

--- Coursework/processing.py
+++ Coursework/processing.py
@@ -9,13 +9,13 @@ def s(times):
 
 
 def e(s_):
+    threads = np.arange(1, 5)
     temp = [s_[i] / threads[i] for i in range(len(s_))]
     print(temp)
     return temp
 
 
 indexes = ['N=1', 'N=2', 'N=3', 'N=4']
-threads = np.arange(1, 5)
 
 times_openmp_j_41 = np.array([0.3504, 0.3307, 0.3041, 0.2805])
 times_openmp_j_71 = np.array([1.5075, 1.0316, 0.9943, 0.9564])
@@ -31,17 +31,17 @@ Times = [times_openmp_j_41, times_openmp_j_71, times_openmp_j_101,
          times_openmp_gz_41, times_openmp_gz_71, times_openmp_gz_101,
          times_mpi_j_41, times_mpi_j_71, times_mpi_j_101]
 
-s_openmp_j_41 = s(times_openmp_j_41)
-s_openmp_j_71 = s(times_openmp_j_71)
-s_openmp_j_101 = s(times_openmp_j_101)
-s_openmp_gz_41 = s(times_openmp_gz_41)
-s_openmp_gz_71 = s(times_openmp_gz_71)
-s_openmp_gz_101 = s(times_openmp_gz_101)
-s_mpi_j_41 = s(times_mpi_j_41)
-s_mpi_j_71 = s(times_mpi_j_71)
-s_mpi_j_101 = s(times_mpi_j_101)
+s_openmp_j_41 = s(Times[0])
+s_openmp_j_71 = s(Times[1])
+s_openmp_j_101 = s(Times[2])
+s_openmp_gz_41 = s(Times[3])
+s_openmp_gz_71 = s(Times[4])
+s_openmp_gz_101 = s(Times[5])
+s_mpi_j_41 = s(Times[6])
+s_mpi_j_71 = s(Times[7])
+s_mpi_j_101 = s(Times[8])
 
-print('='*10)
+print('=' * 10)
 
 e_openmp_j_41 = e(s_openmp_j_41)
 e_openmp_j_71 = e(s_openmp_j_71)
@@ -54,9 +54,9 @@ e_mpi_j_71 = e(s_mpi_j_71)
 e_mpi_j_101 = e(s_mpi_j_101)
 
 # 41x41
-plt.scatter(indexes, times_openmp_j_41, label='Jacobi, OpenMP, 41x41', s=60, c='b')
-plt.scatter(indexes, times_openmp_gz_41, label='Gauss–Seidel, OpenMP, 41x41', s=60, c='r')
-plt.scatter(indexes, times_mpi_j_41, label='Jacobi, MPI, 41x41', s=60, c='g')
+plt.scatter(indexes, Times[0], label='Jacobi, OpenMP, 41x41', s=60, c='b')
+plt.scatter(indexes, Times[3], label='Gauss–Seidel, OpenMP, 41x41', s=60, c='r')
+plt.scatter(indexes, Times[6], label='Jacobi, MPI, 41x41', s=60, c='g')
 plt.ylabel("t, s")
 plt.legend()
 plt.grid(True)
@@ -79,9 +79,9 @@ plt.grid(True)
 plt.show()
 
 # 71x71
-plt.scatter(indexes, times_openmp_j_71, label='Jacobi, OpenMP, 71x71', s=60, c='b')
-plt.scatter(indexes, times_openmp_gz_71, label='Gauss–Seidel, OpenMP, 71x71', s=60, c='r')
-plt.scatter(indexes, times_mpi_j_71, label='Jacobi, MPI, 71x71', s=60, c='g')
+plt.scatter(indexes, Times[1], label='Jacobi, OpenMP, 71x71', s=60, c='b')
+plt.scatter(indexes, Times[4], label='Gauss–Seidel, OpenMP, 71x71', s=60, c='r')
+plt.scatter(indexes, Times[7], label='Jacobi, MPI, 71x71', s=60, c='g')
 plt.ylabel("t, s")
 plt.legend()
 plt.grid(True)
@@ -104,9 +104,9 @@ plt.grid(True)
 plt.show()
 
 # 101x101
-plt.scatter(indexes, times_openmp_j_101, label='Jacobi, OpenMP, 101x101', s=60, c='b')
-plt.scatter(indexes, times_openmp_gz_101, label='Gauss–Seidel, OpenMP, 101x101', s=60, c='r')
-plt.scatter(indexes, times_mpi_j_101, label='Jacobi, MPI, 101x101', s=60, c='g')
+plt.scatter(indexes, Times[2], label='Jacobi, OpenMP, 101x101', s=60, c='b')
+plt.scatter(indexes, Times[5], label='Gauss–Seidel, OpenMP, 101x101', s=60, c='r')
+plt.scatter(indexes, Times[8], label='Jacobi, MPI, 101x101', s=60, c='g')
 plt.ylabel("t, s")
 plt.legend()
 plt.grid(True)
@@ -136,9 +136,9 @@ labels = ['41x41', '71x71', '101x101']
 bar_width = 0.25
 
 fig, ax = plt.subplots()
-bar1 = ax.bar([i - bar_width for i in range(3)], [cnt[i] for i in [0, 1, 2]], bar_width, label='Jacobi, OpenMP')
-bar2 = ax.bar([i for i in range(3)], [cnt[i] for i in [3, 4, 5]], bar_width, label='Gauss–Seidel, OpenMP')
-bar3 = ax.bar([i + bar_width for i in range(3)], [cnt[i] for i in [6, 7, 8]], bar_width, label='Jacobi, MPI')
+bar1 = ax.bar([i - bar_width for i in range(3)], [cnt[i] for i in range(3)], bar_width, label='Jacobi, OpenMP')
+bar2 = ax.bar([i for i in range(3)], [cnt[i] for i in range(3, 6)], bar_width, label='Gauss–Seidel, OpenMP')
+bar3 = ax.bar([i + bar_width for i in range(3)], [cnt[i] for i in range(6, 9)], bar_width, label='Jacobi, MPI')
 ax.set_xticks(range(3))
 ax.set_xticklabels(labels)
 ax.set_ylabel('Сетка')
@@ -149,9 +149,9 @@ plt.grid(True)
 plt.show()
 
 fig, ax = plt.subplots()
-bar1 = ax.bar([i - bar_width for i in range(3)], [cnt_sec[i][3] for i in [0, 1, 2]], bar_width, label='Jacobi, OpenMP')
-bar2 = ax.bar([i for i in range(3)], [cnt_sec[i][3] for i in [3, 4, 5]], bar_width, label='Gauss–Seidel, OpenMP')
-bar3 = ax.bar([i + bar_width for i in range(3)], [cnt_sec[i][3] for i in [6, 7, 8]], bar_width, label='Jacobi, MPI')
+bar1 = ax.bar([i - bar_width for i in range(3)], [cnt_sec[i][3] for i in range(3)], bar_width, label='Jacobi, OpenMP')
+bar2 = ax.bar([i for i in range(3)], [cnt_sec[i][3] for i in range(3, 6)], bar_width, label='Gauss–Seidel, OpenMP')
+bar3 = ax.bar([i + bar_width for i in range(3)], [cnt_sec[i][3] for i in range(6, 9)], bar_width, label='Jacobi, MPI')
 ax.set_xticks(range(3))
 ax.set_xticklabels(labels)
 ax.set_ylabel('Сетка')
